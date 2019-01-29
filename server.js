fd459d494a514c01630e1e6475e571bdeacba1a3
Jack Li <lijaack@yahoo.com>
2019-01-29 12:42:43 -0800

    initial files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// // Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});

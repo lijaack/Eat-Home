2ef2969be4645d37216c2899c5bd5427a4b48f7e
Jack Li <lijaack@yahoo.com>
2019-01-31 14:04:34 -0800

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './pages/login';
 import Signup from './pages/signup';
+import Test from './pages/test';
+
 
 
 function App() {
@@ -10,7 +12,7 @@ function App() {
       <div>
         {/* <Nav /> */}
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={Test} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/books/:id" component={Signup} />
           {/* <Route component={NoMatch} /> */}
---
+++ client/src/pages/login.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+            <Col size="md-3"></Col>
+            <Col size="md-6">
+                <h3>Login Form</h3>
+                <form>
+                    <label for="username">Username:</label>
+                    <Input
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                        name="username"
+                        placeholder="Username"
+                    />
+                     <label for="password">Password:</label>
+                    <Input
+                        type="password"
+                        value={this.state.Password}
+                        onChange={this.handleInputChange}
+                        name="password"
+                        placeholder="Password"
+                    />
+                     <FormBtn
+                        disabled={!(this.state.username && this.state.password)}
+                        onClick={this.handleFormSubmit}
+                    >
+                        Login
+                    </FormBtn>
+                </form>
+            </Col>
+            <Col size="md-3"></Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Login;

5e35815665c26ac832c66ad4e2e0275cc63685b5
Jack Li <lijaack@yahoo.com>
2019-01-31 14:28:49 -0800

    added nav bar + sign up form

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Test from './pages/test';
-
+import Nav from './components/Nav'
 
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Nav /> */}
+        <Nav />
         <Switch>
           <Route exact path="/" component={Test} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/books/:id" component={Signup} />
+          <Route exact path="/signup" component={Signup} />
           {/* <Route component={NoMatch} /> */}
         </Switch>
       </div>

a2cc525e9c5459a07948146fca30398ec258beca
Jack Li <lijaack@yahoo.com>
2019-01-31 13:41:31 -0800

    added passport-local to dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import Test from './pages/test'
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from './pages/login';
+import Signup from './pages/signup';
 
-class App extends Component {
-  render() {
-    return (
-      <Test/>
-    );
-  }
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* <Nav /> */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/books/:id" component={Signup} />
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

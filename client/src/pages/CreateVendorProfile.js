6d4811f944303b56cee75e7a93c059885b0bac2d
Arnesh Regmi <arneshregmi@Arneshs-MacBook-Pro.local>
2019-02-01 09:43:54 -0800

    Create Profile Page created

--- client/src/pages/CreateVendorProfile.js
+++ client/src/pages/CreateVendorProfile.js
@@ -9,19 +9,34 @@ import Images from "../components/Images"
 //Profile page shows 1 restaurant
 //state 
 class Profile extends Component {
+
     state = {
         businessName: "",
         license: "",
-        password: "",
-        About: "",
-        Photos: "",
+        about: "",
+        photo: ""
     };
 
-//render
+handleFormSubmit = event => {
+    //api call to post info to database to database
 
-//onsubmit change state....
 
+    //Empty the text fields
+    this.setState({
+        businessName: "",
+        license: "",
+        about: "",
+        photo: ""
+      });
+}
 
+handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
 
 render() {
     return (
@@ -29,14 +44,59 @@ render() {
         <Jumbotron height="100"> 
         <h1> Vendor Create Profile</h1>
         </Jumbotron>
+        <Row>
         <Col size = "md-6">
         <Jumbotron height="100"> 
-        <h1> Vendor Create Profile</h1>
-        </Jumbotron>
-
-
-     
+        <form className="form">
+        <Row>
+        <input
+            value={this.state.businessName}
+            name="businessName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Business Name"
+          />
+          </Row>
+          <Row>
+           <input
+            value={this.state.license}
+            name="license"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="License #"
+          />
+          </Row>
+         </form>
+        </Jumbotron> 
         </Col>
+        <Col size = "md-6">
+        <Jumbotron height="100">
+        <Row>
+        <input
+            value={this.state.about}
+            name="about"
+            onChange={this.handleInputChange}
+            type="text area"
+            placeholder="About"
+          />
+          </Row>
+          <Row>
+        <input
+            value={this.state.photo}
+            name="photo"
+            onChange={this.handleInputChange}
+            type="text area"
+            placeholder="photo"
+          />
+          </Row>
+        </Jumbotron>
+        </Col>
+        </Row>
+        <Row>
+            <Col size = "md-12"> 
+            <button onClick={this.handleFormSubmit}>Submit</button>
+           </Col>
+        </Row>
 
     </Container>
     )}

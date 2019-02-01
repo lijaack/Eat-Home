a9e6c26c19368a1bdcb9a4d601adcf0919dd8fc3
Jack <lijaack@yahoo.com>
2019-01-31 22:58:38 -0800

    sign up now form sends data to DB

---
+++ client/src/components/LandingGrid/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function LandingGrid() {
+  return (
+   <div>
+    <ul className="list-unstyled">
+    <li className="media">
+      <img className="mr-3" src="./200x200.svg" alt="Generic placeholder image">
+      <div className="media-body">
+        <h5 className="mt-0 mb-1">List-based media object</h5>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </div>
+    </li>
+    <li className="media my-4">
+      <img className="mr-3" src="./200x200.svg" alt="Generic placeholder image">
+      <div className="media-body">
+        <h5 className="mt-0 mb-1">List-based media object</h5>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </div>
+    </li>
+    <li className="media">
+      <img className="mr-3" src="./200x200.svg" alt="Generic placeholder image">
+      <div className="media-body">
+        <h5 className="mt-0 mb-1">List-based media object</h5>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </div>
+    </li>
+  </ul>
+    </div>
+      );
+    }
+   
+    export default LandingGrid;
--- controllers/eatHomeController.js
+++ controllers/eatHomeController.js
@@ -1,34 +1,29 @@
 const db = require("../models");
 
 module.exports = app => {
-  
-    app.post("/api/books", function(req,res){
+
+    app.post("/signup", function(req,res){
         console.log("hello")
-        db.User.create({
-        }).then(function(data) {
+        db.User.create(
+            req.body
+        ).then(function(data) {
             console.log("data")
         });
     }); 
 
+    app.get("/api/books",function(req,res){
+        console.log("finding user location and appending local spots")
+        db.Restaurant.findAll({ 
+            where: { location: "IP location, please add"}
+        }).then(restaurants => {
+            //return restaurants local to the area
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        })
+    });
+        
+        
+    app.post("/api/books",function(req,res){
+        
+    })
 
 };

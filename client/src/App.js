1858baa54743fa928e37e87d8ad837306af04c8b
Oleg Hansen <oleg-ucb@outlook.com>
2019-02-02 12:16:09 -0800

    first version of landing page now rendering as components

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/login';
 import Signup from './pages/signup';
 import Test from './pages/test';
 import Nav from './components/Nav'
+import LandingMiddle from './components/LandingMiddle'
 
 
 function App() {
@@ -13,9 +14,11 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Test} />
+          {/* <Route exact path="/" component={Test} /> */}
+          <Route exact path="/" component={LandingMiddle}/>
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          {/* <Route exact path="/landing" component={LandingMiddle}/> */}
           {/* <Route component={NoMatch} /> */}
         </Switch>
       </div>

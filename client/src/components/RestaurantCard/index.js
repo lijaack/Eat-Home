46f5342df5555659a20f518c990cb900c62f6a80
Jack <lijaack@yahoo.com>
2019-02-03 23:51:53 -0800

    added card component to view restaurants

---
+++ client/src/components/RestaurantCard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function RestaurantCard(props) {
+    return (
+     
+        <div class="card h-100">
+            <img class="card-img-top" src="..." alt="Card image cap"/>
+            <a href=""><img class="card-img-top" src="" alt="Card image cap"/></a>
+            <div class="card-body">
+                <h5 class="card-title"><a href=""></a></h5>
+                <p class="card-text"></p>
+                <p class="card-text"></p>
+
+                <div id=""><a href="" class="btn btn-primary">View Restaurant</a></div>
+            </div>
+        </div>
+
+    );
+}
+
+export default RestaurantCard;
--- client/src/pages/restaurants/restaurants.js
+++ client/src/pages/restaurants/restaurants.js
@@ -1,6 +1,7 @@
 import API from "../../utils/API";
 import React, { Component } from "react";
 import { Col, Row, Container} from "../../components/Grid";
+import RestaurantCard from "../../components/RestaurantCard";
 import "./style.css"
 
 
@@ -49,42 +50,9 @@ class Restaurants extends Component {
         <Container>
             <Row>
                 <Col size="4">
-                <div className="text-center">
-                    <img src={require('./images/order.png')} alt="order" style={{width:"100px"}}></img>
-                    <h5>Order your meal</h5>
-                    <p>Find a meal that you like, and place your order!</p>
-                </div>
-                </Col>
-                <Col size="4">
-                <div className="text-center">                  
-                    <img src={require('./images/cook.png')} alt="cook" style={{width:"100px"}}></img>
-                    <h5>Wait for your meal to be cooked</h5>
-                    <p>A chef will prepare your home cooked meal.</p>
-                </div>
-                </Col>
-                <Col size="4">
-                <div className="text-center">                  
-
-                    <img src={require('./images/pickup.png')} alt="pick up" style={{width:"100px"}}></img>
-                    <h5>Pick up your order!</h5>
-                    <p>When the chef is done, you will get a message letting you know. Simply go and pick up your order.</p>
-                </div>
+                    <RestaurantCard/>
                 </Col>
             </Row>
-            <Row>
-                <Col size="4"/>
-                <Col size="4">  
-                <div className="text-center">                  
-                    <button type="button" className="btn btn-success">Search for a meal</button>
-                </div>
-                </Col>
-                <Col size="4"/>
-            </Row>
-            <h2 className="text-center">Popular Meals</h2>
-            <Row>
-                
-            </Row>
-
         </Container>                
     </div>
     );

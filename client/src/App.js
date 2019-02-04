0fd1feb253842f11c7e749407005080e351b69f9
Oleg Hansen <oleg-ucb@outlook.com>
2019-02-04 10:14:45 -0800

    commiting again to update after pull

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ import Signup from './pages/signup';
 import Home from './pages/home/home';
 import {NavUser, Nav} from './components/Nav'
 import LandingMiddle from './components/LandingMiddle'
-<<<<<<< HEAD
 import LandingAlt from './components/LandingAlt'
-
+import API from "./utils/API";
+import Restaurants from './pages/restaurants/restaurants';
 
 function App() {
   return (
@@ -29,9 +29,7 @@ function App() {
       </div>
     </Router>
   );
-=======
-import API from "./utils/API";
-import Restaurants from './pages/restaurants/restaurants'
+
 
 
 class App extends Component {
@@ -66,7 +64,6 @@ class App extends Component {
         </Router>
       )
     }
->>>>>>> 945514172c4999f222e1f5f9a5c751f2b714f6dd
 
     //route access restricted to the following routes if user is not logged in
     return(

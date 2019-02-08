aea278bbbb1c9f35e3a7023d1679d942b882bf2d
Jack <lijaack@yahoo.com>
2019-02-07 22:42:07 -0800

    adding react response to server for production

--- server.js
+++ server.js
@@ -42,6 +42,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 // Routes
 // =============================================================
